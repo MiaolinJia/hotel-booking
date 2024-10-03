012c42f659307284fec04f25192642d6cd20c2eb
Teng Luo <luoteng91@gmail.com>
2024-10-03 02:34:40 -0400

    Init commit

--- ui-service/src/main/frontend/hotel-booking-ui/src/pages/HomePage.tsx
+++ ui-service/src/main/frontend/hotel-booking-ui/src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ const HomePage: React.FC = () => {
   return (
     <HomePageExampleComponent>
       <h1>"Here is a placeholder for homepage components"</h1>
+    {/*    starting point */}
     </HomePageExampleComponent>
   );
 };
