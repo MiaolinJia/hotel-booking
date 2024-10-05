e8f2721eff5f00b036c5dc12ee49fe86ab8a6d8e
Teng Luo <luoteng91@gmail.com>
2024-10-05 17:31:49 -0400

    Rename component

--- ui-service/src/main/frontend/hotel-booking-ui/src/pages/HomePage.tsx
+++ ui-service/src/main/frontend/hotel-booking-ui/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import HomePageExampleComponent from "../components/HomePageExampleComponent";
-import SearchComponents from "../components/search-components/SearchComponent.tsx";
+import SearchHotelComponent from "../components/search-components/SearchComponent.tsx";
 
 const HomePage: React.FC = () => {
     return (
@@ -7,7 +7,7 @@ const HomePage: React.FC = () => {
             <HomePageExampleComponent>
                 <h1>"Here is a placeholder for homepage components"</h1>
             </HomePageExampleComponent>
-            <SearchComponents></SearchComponents>
+            <SearchHotelComponent/>
         </>
     );
 };
